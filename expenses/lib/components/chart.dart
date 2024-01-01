90a8ae9a07fd879a3c78f93fe392db397dc76ace
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-01 10:29:59 -0300

    Alterações grandes

---
+++ expenses/lib/components/chart.dart
@@ -0,0 +1,48 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weeDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weeDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weeDay.month;
+        bool sameYear = recentTransaction[i].date.year == weeDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weeDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    groupedTransactions;
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactions.map((tr) {
+          return Text('${tr['day']}: ${tr['value']}');
+        }).toList(),
+      ),
+    );
+  }
+}
