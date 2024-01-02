5c5cc16ed8726627fab28e0fc97e5d5336dee865
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-02 20:43:28 -0300

    Alterações

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -30,7 +30,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weeDay)[0],
         'value': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get _weekTotalValue {
