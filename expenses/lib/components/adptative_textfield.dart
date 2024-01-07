4f342b15309155ef5cba17bb5ccacd1d84141fb8
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-07 09:56:17 -0300

    Alterações

--- expenses/lib/components/adptative_textfield.dart
+++ expenses/lib/components/adptative_textfield.dart
@@ -4,34 +4,55 @@ import 'dart:io';
 
 class AdaptativeTextField extends StatelessWidget {
   final Function submitForm;
-  final TextEditingController valueController;
+  final TextEditingController varController;
+  final bool isNum;
 
   AdaptativeTextField({
     required this.submitForm,
-    required this.valueController,
+    required this.varController,
+    required this.isNum,
   });
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
-        ? CupertinoTextField(
-            controller: valueController,
-            onSubmitted: (_) => submitForm(),
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            prefix: Text(
-              'value',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          )
-        : TextField(
-            controller: valueController,
-            onSubmitted: (_) => submitForm(),
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            decoration: InputDecoration(
-              labelText: 'Valor (R\$)',
-            ),
-          );
+        ? isNum
+            ? CupertinoTextField(
+                controller: varController,
+                onSubmitted: (_) => submitForm(),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                prefix: Text(
+                  'Valor (R\$)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            : CupertinoTextField(
+                controller: varController,
+                onSubmitted: (_) => submitForm(),
+                prefix: Text(
+                  'Título',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+        : isNum
+            ? TextField(
+                controller: varController,
+                onSubmitted: (_) => submitForm(),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)',
+                ),
+              )
+            : TextField(
+                controller: varController,
+                onSubmitted: (_) => submitForm(),
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+              );
   }
 }
