5ec80c0df8e60cf688c09a89b31cd9013af77a34
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-08 12:46:18 -0300

    Alterações Pontuais

--- expenses/lib/components/adptative_button.dart
+++ expenses/lib/components/adptative_button.dart
@@ -4,20 +4,21 @@ import 'dart:io';
 
 class AdaptativeButton extends StatelessWidget {
   final String label;
-  final Function onPressed;
+  final Function() onPressed;
 
   AdaptativeButton({
     required this.label,
     required this.onPressed,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
         ? CupertinoButton(
             child: Text(label),
-            onPressed: onPressed(),
-            color: Theme.of(context).primaryColor,
+            onPressed: onPressed,
+            color: Theme.of(context).colorScheme.primary,
             padding: EdgeInsets.symmetric(
               horizontal: 20,
             ),
@@ -25,12 +26,11 @@ class AdaptativeButton extends StatelessWidget {
         : ElevatedButton(
             child: Text(
               label,
-              style: TextStyle(color: Theme.of(context).primaryColor),
+              style: TextStyle(
+                color: Theme.of(context).textTheme.button?.color,
+              ),
             ),
-            onPressed: onPressed(),
-            style: TextButton.styleFrom(
-              backgroundColor: Theme.of(context).primaryColor,
-            ),
+            onPressed: onPressed,
           );
   }
 }
