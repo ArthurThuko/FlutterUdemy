f5e64b4d78f36a079421f1ffa020d40d27b14caa
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-11 19:35:59 -0300

    Finalizando introdução dart

--- Dart/fundamentos/tiposBasicos2.dart
+++ Dart/fundamentos/tiposBasicos2.dart
@@ -5,10 +5,8 @@ main() {
     'João': '+55 (11) 98765-4321',
     'Arthur': '+5648544854855',
     'Maria': '84894848484',
-    'João': '185191 98765-4321',
   }; //No map não é permitido repetição, será sempre a última chave
 
-  print(telefones is Map);
   print(telefones);
   print(telefones['João']);
   print(telefones.length);
@@ -22,7 +20,6 @@ main() {
     'Flamengo',
     'Cuiabá',
   }; //Set não aceita repetições
-  print(times is Set);
   times.add('Palmeiras');
   print(times);
   print(times.contains('Vasco'));
