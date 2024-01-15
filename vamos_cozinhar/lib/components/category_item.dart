789df88e41f8d31f260edfe2f01ca307d2396a87
ArthurThuko <arthurhenriqsfc@gmail.com>
2024-01-15 19:27:23 -0300

    Novo projeto Vamos Cozinhar

--- vamos_cozinhar/lib/components/category_item.dart
+++ vamos_cozinhar/lib/components/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vamos_cozinhar/utils/app_routes.dart';
 import '../models/category.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -6,10 +7,37 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(category.title),
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
     );
   }
 }
--- vamos_cozinhar/lib/main.dart
+++ vamos_cozinhar/lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:vamos_cozinhar/screens/category_screen.dart';
- 
+import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleMedium: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: CategoryScreen(),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoryScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
